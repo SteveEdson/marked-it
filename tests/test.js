4d8c08861407193e7eba4541d5b9b17e113e4110
Steve Edson <steve.edson@jaywing.com>
2018-08-14 13:06:08 +0100

    new tests

--- tests/test.js
+++ tests/test.js
@@ -5,3 +5,12 @@ test('Emoji output', t => {
     const output = md.render(':beer:');
     t.snapshot(output);
 });
+
+test('Custom warning container', t => {
+    const input = `::: warning
+    This is a warning!
+    :::`;
+
+    const output = md.render(input);
+    t.snapshot(output);
+});
